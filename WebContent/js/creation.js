4b6aeb030986f595ab16945adc61128563facbaa
adumas <adumas@eleves.ec-nantes.fr>
2014-12-24 13:43:10 +0100

    Ajout d'une fonction d'ajout de reponse à une question

--- WebContent/js/creation.js
+++ WebContent/js/creation.js
@@ -2,6 +2,38 @@
  * 
  */
 
-function ajoutQuestion(){
-	$(this.el).parent().setBackgroundColor("red");
+function ajoutReponse(elmnt){
+	//elmnt.parentNode.parentNode.parentNode.style.backgroundColor="red";
+	
+	/*var question = document.createElement('p');
+	question.id = "reponse";
+	var texte = document.createTextNode("Reponse: ");
+	
+	var question = document.createElement('p');
+	question.id = "reponse";
+	var texte = document.createTextNode("Reponse: ");
+	var inputText = document.createElement('input');
+	inputText.type="text";
+	inputText.name="testName";
+	inputText.id="reponseInput";
+	texte.appendChild(inputText);
+	question.appendChild(texte);*/
+	
+	var reponse = document.getElementById("reponse");
+	var reponse2 = reponse.cloneNode(true);
+	
+	elmnt.parentNode.parentNode.parentNode.appendChild(reponse2);
 };
+
+
+
+
+/*
+<reponses>
+<p id="reponse">
+	Reponse: <input type="text" name="r1.1" id="reponseInput"/>
+	<span id="checkbox">Bonne reponse?<input type="checkbox" name="c1"/></span>
+	<span id="add"><input type="button" name="addQ" value="Ajouter reponse" onclick="ajoutReponse(this)" /></span>
+</p>
+</reponses>
+*/
