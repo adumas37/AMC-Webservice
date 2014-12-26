4aab54b868cb1c1c92e281ec570cab17d8aad22d
adumas <adumas@eleves.ec-nantes.fr>
2014-12-26 11:48:59 +0100

    Ajout de la fonction de creation d'entete du fichier latex

--- src/m2/hw/Creation.java
+++ src/m2/hw/Creation.java
@@ -13,7 +13,51 @@ public class Creation {
 	public void creation(String data){
 		System.out.println(data);
 		/* Exemple de data pour un questionnaire à 2question, 2 rep pour la première et une pour la deuxieme question:
-		 * question=LA+question+1&reponse=rep1.1&bonne=on&reponse=rep1.2&question=LA+question+2&reponse=rep2.1&bonne=on&submit=Creer+questionnaire
+		 * matiere=maths&date=26%2F12%2F2014&question=1%2B1%3D&reponse=1&reponse=2&bonne=on&question=La+question+facile&reponse=oui&bonne=on&submit=Creer+questionnaire
 		 */
+		System.out.println(formerEntete("maths","12/12/12"));
+		ecrireFichier();
+	}
+	
+	private void ecrireFichier(){
+		
+	}
+	
+	
+	private String formerEntete(String matiere, String date){
+		String entete= new String();
+		
+		entete = "%%% En-tête des copies \n" 
+				+"\\noindent{\\bf QCM  \\hfill TEST}\n\n"
+			
+				+"\\vspace*{.5cm}\n"
+				+"\\begin{minipage}{.4\\linewidth}\n"
+				+"\\centering\\large\\bf "+ matiere +"\\ Examen du " +date +"\\end{minipage}\n"
+				+"\\champnom{\\fbox{\n"
+					            +"\t\\begin{minipage}{.5\\linewidth}\n"
+					            +"\tNom et prénom :\n\n"
+			
+					            +"\t\\vspace*{.5cm}\\dotfill\n"
+					            +"\t\\vspace*{1mm}\n"
+					            +"\t\\end{minipage}\n"
+					            +"}}\n\n"
+			
+				+"\\begin{center}\\em\n"
+				+"Durée : 10 minutes.\n\n"
+			
+				  +"Aucun document n'est autorisé.\n"
+				  +"L'usage de la calculatrice est interdit.\n\n"
+			
+				  +"Les questions faisant apparaître le symbole \\multiSymbole{} peuvent\n"
+				  +"présenter zéro, une ou plusieurs bonnes réponses. Les autres ont\n"
+				  +"une unique bonne réponse.\n\n"
+			
+				  +"Des points négatifs pourront être affectés à de \\emph{très\n"
+					+"mauvaises} réponses.\n"
+				+"\\end{center}\n"
+				+"\\vspace{1ex}\n";
+		
+		
+		return entete;
 	}
 }
