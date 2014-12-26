5047a93cdc840bab42620f3511c2ae26011cbfa8
adumas <adumas@eleves.ec-nantes.fr>
2014-12-26 10:13:33 +0100

    ajout de la classe de creation de questionnaire

---
+++ src/m2/hw/Creation.java
@@ -0,0 +1,16 @@
+package m2.hw;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("creation")
+public class Creation {
+
+	@POST
+	public void creation(String data){
+		System.out.println(data);
+	}
+}
