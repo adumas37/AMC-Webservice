5f4cbf9397c75bb3eae1c1ea156c03c61ccabf8c
adumas <adumas@eleves.ec-nantes.fr>
2014-12-26 10:35:44 +0100

    passage de id vers class pour les blocs html (non unicité)

--- src/m2/hw/Creation.java
+++ src/m2/hw/Creation.java
@@ -12,5 +12,8 @@ public class Creation {
 	@POST
 	public void creation(String data){
 		System.out.println(data);
+		/* Exemple de data pour un questionnaire à 2question, 2 rep pour la première et une pour la deuxieme question:
+		 * q1=LA+question+1&r1.1=LA+reponse+1.1&c1=on&r1.1=LA+reponse+1.2&c1=on&q1=LA%2Bquestion+2&r1.1=LA+reponse+2.1&c1=on&submit=Creer+questionnaire
+		 */
 	}
 }
