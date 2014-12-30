b0989608d1f19d8ab632e17a0961a0a8963dd4a7
adumas <adumas@eleves.ec-nantes.fr>
2014-12-30 17:46:07 +0100

    modification de noms de fichier en consequence

--- src/m2/hw/CreationProjet.java
+++ src/m2/hw/CreationProjet.java
@@ -1,5 +1,15 @@
 package m2.hw;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+
+
+@Path("creationProjet")
 public class CreationProjet {
 
+	@GET
+	public void creationRepertoire(String nom){
+		
+	}
 }
