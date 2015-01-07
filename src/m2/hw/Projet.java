e4ab67f3942d9e47c4284b35345657b810ac64b1
adumas <adumas@eleves.ec-nantes.fr>
2015-01-07 21:44:02 +0100

    ajout de la fonction de telechargement d'un fichier pdf

--- src/m2/hw/Projet.java
+++ src/m2/hw/Projet.java
@@ -1,5 +1,27 @@
 package m2.hw;
 
+import java.io.File;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
+
 public class Projet {
 
+	@Path("pdf")
+	@GET
+	@Produces({"application/pdf"})
+	public Response getFile() {
+		 
+		File file = new File("/Projets-QCM/test4/DOC-sujet.pdf");
+ 
+		ResponseBuilder response = Response.ok((Object) file);
+		response.header("Content-Disposition",
+			"attachment; filename=new-excel-file.xls");
+		return response.build();
+ 
+	}
+	
 }
