607faa2ff70a60f5f3095e87336fce1a8e70eb84
adumas <adumas@eleves.ec-nantes.fr>
2015-01-08 15:20:39 +0100

    recuperation et stockage des copies envoyees par l'utilisateur

--- src/m2/hw/UploadCopies.java
+++ src/m2/hw/UploadCopies.java
@@ -30,19 +30,46 @@ public class UploadCopies {
 		@FormDataParam("classe") String classe,
 		@FormDataParam("file") InputStream uploadedInputStream,
 		@FormDataParam("file") FormDataContentDisposition fileDetail) {
-		File file = new File(PROJECTS_PATH +"/"+PROJECT+"/"+PROJECTS_PATH +PROJECT+"sujet.pdf");
  
 		String fileName = fileDetail.getFileName();
 		
 		System.out.println("classe: "+classe+", file: "+fileName);
 		
-
-		//TODO Changer le lien ci-dessous pour ne plus avoir de chemin fixé
-		URI uri = UriBuilder.fromUri("http://localhost:8080/REST.Test/")
-				.path("{a}")
-				.build("Correction.html");
+		if (!fileName.equals("") && !classe.equals("") && fileName.contains(".pdf")){
+			
+			String uploadedFileLocation = PROJECTS_PATH + "/" + PROJECT + "/copies.pdf";
+			CreationProjet.saveFile(uploadedInputStream, uploadedFileLocation);
+			//TODO recupererClasseCSV(classe);
+			
+	
+			//TODO Changer le lien ci-dessous pour ne plus avoir de chemin fixé
+			URI uri = UriBuilder.fromUri("http://localhost:8080/REST.Test/")
+					.path("{a}")
+					.build("Correction.html");
+			
+			return Response.seeOther(uri).build();
+		}
+		else {
+			if (!fileName.contains(".pdf") && !fileName.equals("")){
+				return Response.status(204).entity("Erreur d'extension du fichier" +
+						"Le fichier doit etre un fichier PDF (.pdf)").build();
+			}
+			if (fileName.equals("") && classe.equals("")){
+				return Response.status(204).entity("Aucun fichier selectionne ou classe selectionne. " +
+						"La correction d'un projet requiert une classe et des copies.").build();
+			}
+			else if (classe.equals("")) {
+				return Response.status(204).entity("Aucun nom specifie. " +
+						"La correction d'un projet requiert une classe.").build();
+			}
+			else if (fileName.equals("")){
+				return Response.status(204).entity("Aucun fichier selectionne. " +
+						"La correction d'un projet requiert des copies.").build();
+			}
+			
+		}
 		
-		return Response.seeOther(uri).build();
+		return Response.status(406).entity("Not reacheable").build();
  
 	}
 	
