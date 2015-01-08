0a5755428a367f4e9d163fe54bc8fafa00445f70
adumas <adumas@eleves.ec-nantes.fr>
2015-01-08 15:01:28 +0100

    modification de la fonction pour recuperer les données

--- src/m2/hw/UploadCopies.java
+++ src/m2/hw/UploadCopies.java
@@ -1,12 +1,21 @@
 package m2.hw;
 
 import java.io.File;
+import java.io.InputStream;
+import java.net.URI;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
 import javax.ws.rs.core.Response.ResponseBuilder;
+
+import com.sun.jersey.core.header.FormDataContentDisposition;
+import com.sun.jersey.multipart.FormDataParam;
 
 @Path("uploadCopies")
 public class UploadCopies {
@@ -14,72 +23,28 @@ public class UploadCopies {
 	private static final String PROJECTS_PATH = "Projets-QCM";
 	private static final String PROJECT = "projetTestWebservice";
 	
-	/**
-	 * Permet de renvoyer les copies a imprimer
-	 * @return
-	 */
-	@Path("copies")
-	@GET
-	@Produces({"application/pdf"})
-	public Response getCopies() {
+
+	@POST
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	public Response Correction(
+		@FormDataParam("classe") String classe,
+		@FormDataParam("file") InputStream uploadedInputStream,
+		@FormDataParam("file") FormDataContentDisposition fileDetail) {
 		File file = new File(PROJECTS_PATH +"/"+PROJECT+"/"+PROJECTS_PATH +PROJECT+"sujet.pdf");
  
-		ResponseBuilder response = Response.ok((Object) file);
-		response.header("Content-Disposition",
-			"attachment; filename=Copies.pdf");
-		return response.build();
+		String fileName = fileDetail.getFileName();
+		
+		System.out.println("classe: "+classe+", file: "+fileName);
+		
+
+		//TODO Changer le lien ci-dessous pour ne plus avoir de chemin fixé
+		URI uri = UriBuilder.fromUri("http://localhost:8080/REST.Test/")
+				.path("{a}")
+				.build("Correction.html");
+		
+		return Response.seeOther(uri).build();
  
 	}
 	
-	/**
-	 * Permet de renvoyer le catalogue des questions
-	 * @return
-	 */
-	@Path("catalog")
-	@GET
-	@Produces({"application/pdf"})
-	public Response getCatalog() {
-		File file = new File(PROJECTS_PATH +"/"+PROJECT+"/"+PROJECTS_PATH +PROJECT+"catalog.pdf");
- 
-		ResponseBuilder response = Response.ok((Object) file);
-		response.header("Content-Disposition",
-			"attachment; filename=Catalogue.pdf");
-		return response.build();
- 
-	}
-	
-	/**
-	 * Permet de renvoyer la correction des questions
-	 * @return
-	 */
-	@Path("corrige")
-	@GET
-	@Produces({"application/pdf"})
-	public Response getCorrige() {
-		File file = new File(PROJECTS_PATH +"/"+PROJECT+"/"+PROJECTS_PATH +PROJECT+"corrige.pdf");
- 
-		ResponseBuilder response = Response.ok((Object) file);
-		response.header("Content-Disposition",
-			"attachment; filename=Corrige.pdf");
-		return response.build();
- 
-	}
-	
-	/**
-	 * Permet de renvoyer le questionnaire en format Latex
-	 * @return
-	 */
-	@Path("questionnaire")
-	@GET
-    @Produces("text/plain")
-    public Response getTextFile() {
-
-		File file = new File(PROJECTS_PATH+"/"+PROJECT+"/"+"questionnaire.tex");
-		ResponseBuilder response = Response.ok((Object) file);
-        response.header("Content-Disposition", "attachment; filename=\"questionnaire.tex\"");
-        return response.build();
-
-
-    }
 	
 }
