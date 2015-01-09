6ce686ca36ee564dbcb6a9fd7e7cdc3a728938de
Erwan <ebriand@eleves.ec-nantes.fr>
2015-01-09 15:40:02 +0100

    Gestion du téléchargement du fichier csv de notes

--- src/m2/hw/Correction.java
+++ src/m2/hw/Correction.java
@@ -1,5 +1,32 @@
 package m2.hw;
 
+import java.io.File;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
+
+
+@Path("correction")
 public class Correction {
 
+	private static String filePath = "exports/notes.csv";
+	
+	@GET
+    @Produces("text/plain")
+    public Response getTextFile() {
+
+		File file = new File(filePath);
+		if (file.exists()){
+			ResponseBuilder response = Response.ok((Object) file);
+	        response.header("Content-Disposition", "attachment; filename=\"notes.csv\"");
+	        return response.build();
+		}
+		else {
+			return null;
+		}
+
+    }
 }
