bc91ebca9664289e8e013d0f556f968950f10e95
adumas <adumas@eleves.ec-nantes.fr>
2015-01-10 11:42:08 +0100

    ajout de la classe Navigation afin de modifier l'action de l'utilisateur

---
+++ src/m2/hw/Navigation.java
@@ -0,0 +1,31 @@
+package m2.hw;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("navigation")
+public class Navigation {
+
+	@Path("ouverture")
+	@GET
+	public void setOuverture(){
+		Utilisateurs.getCurrentUser().setAction("ouverture");
+		System.out.println("Action: ouverture");
+	}
+	@Path("correction")
+	@GET
+	public void setCorrection(){
+		Utilisateurs.getCurrentUser().setAction("correction");
+		System.out.println("Action: correction");
+	}
+	@Path("creation")
+	@GET
+	public void setCreation(){
+		Utilisateurs.getCurrentUser().setAction("creation");
+	}
+	@Path("getAction")
+	public String getAction(){
+		System.out.println("getAction: "+Utilisateurs.getCurrentUser().getAction());
+		return Utilisateurs.getCurrentUser().getAction();
+	}
+}
