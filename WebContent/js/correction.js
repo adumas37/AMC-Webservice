5584aa82b2e6c704772654307f14acc1a34b9161
Erwan <ebriand@eleves.ec-nantes.fr>
2015-01-10 19:20:55 +0100

    Ajout du bouton pour afficher/masquer les notes

--- WebContent/js/correction.js
+++ WebContent/js/correction.js
@@ -1,19 +1,30 @@
 
-function afficherNotes(elmnt){
+function chargerNotes(elmnt){
 	
 	var xhr = new XMLHttpRequest();
 	document.getElementById("resultats").style.display="none";
+	document.getElementById("tableau").style.display="none";
 	xhr.open("POST","rest/correction/notes",false);
 	xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 	
 	xhr.onreadystatechange = function (aEvt){
-		document.getElementById("loading").style.display="none";
-		document.getElementById("resultats").style.display="block";
-		document.getElementById("resultats").innerHTML=xhr.responseText;
+		document.getElementById("tableau").style.display="block";
+		document.getElementById("tableau").innerHTML=xhr.responseText;
 		
 	};
 	xhr.send();
 	
 	
 
+};
+
+function afficherNotes(elmnt){
+	var display = document.getElementById("resultats").style.display;
+	if(display =="none"){
+		document.getElementById("resultats").style.display="block";
+	}
+	else
+	{
+		document.getElementById("resultats").style.display="none";
+	}
 };
