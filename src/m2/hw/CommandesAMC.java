efa53b7140a4f5db65a9a3d1766643aaecc3d76b
adumas <adumas@eleves.ec-nantes.fr>
2015-01-11 11:55:45 +0100

    Ajout de la classe CommandesAMC qui permettra de lancer les differentes commandes d'AMC. Creation de la fonction d'execution des commandes.

---
+++ src/m2/hw/CommandesAMC.java
@@ -0,0 +1,52 @@
+package m2.hw;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import com.sun.istack.internal.logging.Logger;
+
+public class CommandesAMC {
+	
+	private static void executerCommande(String[] amcCmd , String username){
+		try{
+			ProcessBuilder pb = null;
+	        Process p = null;
+
+	        String[] dockerCmd = { "sudo", "docker", "run", "-d", "-v", 
+	        					   "/home/user/Projects:/media/"+username, "dockeramc:v1"};
+	        String[] completeCmd = new String[dockerCmd.length+amcCmd.length];
+	        System.arraycopy(dockerCmd, 0, completeCmd, 0, dockerCmd.length);
+	        System.arraycopy(amcCmd, 0, completeCmd, dockerCmd.length, amcCmd.length);
+	        
+	        pb = new ProcessBuilder(completeCmd);
+	        pb.directory(new File("/media"));
+	
+	        try {
+	            p = pb.start();
+	        } catch (IOException ex) {
+	            Logger.getLogger(Process.class.getName(), null).log(Level.SEVERE, null, ex);
+	        }
+	        
+	        BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+	        BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+	
+	        // read the output from the command
+	        System.out.println("Here is the standard output of the command:\n");
+	
+	        String s = null;
+	        while ((s = stdInput.readLine()) != null) {
+	            System.out.println(s);
+	        }
+	
+	        // read any errors from the attempted command
+	        System.out.println("Here is the standard error of the command (if any):\n");
+	        while ((s = stdError.readLine()) != null) {
+	            System.out.println(s);
+	        }
+	    } catch (IOException ex) {
+	        Logger.getLogger(Process.class.getName(), null).log(Level.SEVERE, null, ex);
+	    }
+	}
+}
