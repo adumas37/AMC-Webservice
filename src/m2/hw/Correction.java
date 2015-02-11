61eef75060fe815a080ab8d0127efd0129e31d54
adumas <adumas@eleves.ec-nantes.fr>
2015-02-11 23:45:49 +0100

    Ajout de la fonction d'ajout en java et modification de la fonction d'appel dans la page html.

--- src/m2/hw/Correction.java
+++ src/m2/hw/Correction.java
@@ -3,17 +3,27 @@ package m2.hw;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
 import java.util.ArrayList;
+import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.core.Response.ResponseBuilder;
 import com.google.gson.Gson;
+import com.sun.jersey.multipart.FormDataBodyPart;
+import com.sun.jersey.multipart.FormDataMultiPart;
 
 
 @Path("correction")
@@ -120,5 +130,64 @@ public class Correction {
 		}
 		
     }
+	
+	@Path("ajouterCopies")
+	@POST
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	public Response ajouterCopies(
+		FormDataMultiPart formParams,
+		@Context UriInfo context) {
+
+		String projectPath = Utilisateurs.getCurrentUser().getProjectPath();
+		
+	    List<FormDataBodyPart> files = formParams.getFields("file");
+	    
+	    for (FormDataBodyPart file : files){	//Recuperer et enregistrer les fichiers de copies
+	    	
+	    	String fileName =  file.getContentDisposition().getFileName();
+	    	if (!fileName.equals("") && fileName.contains(".pdf")){
+		    	InputStream fileInputStream = file.getValueAs(InputStream.class);
+		    	String uploadedFileLocation = projectPath + "/copies/"+fileName;
+				CreationProjet.saveFile(fileInputStream, uploadedFileLocation);
+				
+	    	}
+	    	
+	    }
+	    
+	    try{	//Ecriture du fichier contenant la liste des fichiers des copies
+	    	File file = new File(Utilisateurs.getCurrentUser().getProjectPath()+"copies/listeCopies.txt");
+		    FileWriter fw = new FileWriter(file,true);
+	        for (int i = 0; i < files.size(); i++){
+	        	fw.write("\n"+files.get(i).getContentDisposition().getFileName());
+	        }
+	        fw.close();
+	    }
+	    catch(Exception e){
+	        e.printStackTrace();
+	    }
+	    			
+		//TODO recupererClasseCSV(classe);
+		/*System.out.println("Layout");
+		CommandesAMC.creationLayout(projectPath);
+		System.out.println("generationImagesCopies");
+		CommandesAMC.generationImagesCopies(projectPath);
+		System.out.println("analyseReponses");
+		CommandesAMC.analyseReponses(projectPath);
+		System.out.println("notation");
+		CommandesAMC.notation(projectPath);
+		//System.out.println("associationAuto");
+		//CommandesAMC.associationAuto(projectPath);
+		System.out.println("extractionNotesEleves");
+		CommandesAMC.extractionNotesEleves(projectPath);*/
+		
+
+		String url = context.getBaseUri().toString();
+		url = url.substring(0,url.length()-5); //Supression du "rest/" a la fin de l'url
+		URI uri = UriBuilder.fromUri(url)
+				.path("{a}")
+				.build("Correction.html");
+		
+		return Response.seeOther(uri).build();
+	}	
 	
 }
