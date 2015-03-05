c1e15656713e2db8b3fcd02cfc7ec3de1639aaa0
adumas <adumas@eleves.ec-nantes.fr>
2015-03-05 15:41:16 +0100

    Ajout de la fonction pour recuperer le lien de correction, soit la page d'upload, soit la page de modification de la correction.

--- src/m2/hw/Navigation.java
+++ src/m2/hw/Navigation.java
@@ -1,8 +1,17 @@
 package m2.hw;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.net.URI;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
 
 @Path("navigation")
 public class Navigation {
@@ -56,5 +65,38 @@ public class Navigation {
 	@POST
 	public void setProject(String project){
 		Utilisateurs.getCurrentUser().setProject(project);
+	}
+	
+	
+	
+	@Path("correctionLien/{name}")
+	@GET
+	public Response setCorrection(@PathParam("name") String name,@Context UriInfo context){
+		String url = context.getBaseUri().toString();
+		url=url.substring(0,url.length()-5);
+		URI uri = UriBuilder.fromUri(url)
+				 .path("{a}")
+				 .build("UploadCopies.html");;
+		
+		try{	//Ecriture du fichier contenant la liste des fichiers des copies
+	    	File file = new File(Utilisateurs.getCurrentUser().getProjectPath()+"copies/listeCopies.txt");
+	    	FileWriter fw = new FileWriter(file,true);
+	    	if (file.exists()){
+	        	if(file.length()>3)
+		        	uri = UriBuilder.fromUri(url)
+		    				.path("{a}")
+		    				.build("Correction.html");
+	        	
+	        }
+	    	
+	    	
+	        fw.close();
+	    }
+	    catch(Exception e){
+	        e.printStackTrace();
+	    }
+
+		
+		return Response.seeOther(uri).build();
 	}
 }
