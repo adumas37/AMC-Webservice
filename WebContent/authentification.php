a5621a0e1a571319b54b6739e907238b3f2e889e
RaphaelMeyer6 <raphael.meyer6@gmail.com>
2015-03-11 20:33:31 +0100

    Ajout du code pour la verification LDAP(en attendant le login/pwd est AMC/AMC)

--- WebContent/authentification.php
+++ WebContent/authentification.php
@@ -2,14 +2,25 @@
 session_start();
 $_SESSION['logAttempt']=true;
 if (isset($_POST['username'])){
-	if ($_POST['username']=="AMC"){
-		$_SESSION['username']=$_POST['username'];
-		$_SESSION['logAttempt']=false;
-		header('location: index.php');
+	$Serveur = "ldap://rldap.ec-nantes.fr";
+	$Liaison_LDAP = ldap_connect($Serveur);
+	/*if ($Liaison_LDAP) {
+		// Le serveur est accessible
+		$LDAP_DN="uid=$login, ou=people, dc=ec-nantes, dc=fr";
+		$LDAPBind_User = ldap_bind($Liaison_LDAP, $LDAP_DN, $password);
+		if ($LDAPBind_User)*/
+	if ($_POST['username']=="AMC" && $_POST['password']=="AMC"){
+			// Utilisateur authentifie sur le serveur LDAP
+			//ldap_close($Liaison_LDAP); Enlever le comment losque le serveur sera sur le réseau ECN
+			$_SESSION['username']=$_POST['username'];
+			$_SESSION['logAttempt']=false;
+			header('location: index.php');
 	}
 	else {
-		header('location: identification.php');
-	}
+			// Utilisateur non authentifie
+			header('location: identification.php');
+		}
+	
 }
 elseif (isset ($_SESSION['username'])){
 	header('location: index.php');
