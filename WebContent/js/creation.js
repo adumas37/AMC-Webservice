f29759d07e9ff9077d6fdbc239c0a7c164ae39b0
adumas <adumas@eleves.ec-nantes.fr>
2015-03-12 15:41:45 +0100

    Ajout des boutons d'ajout de question sur la page de modification du questionnaire, et debug de l'ajout de question qui ne marchait pas.

--- WebContent/js/creation.js
+++ WebContent/js/creation.js
@@ -115,11 +115,13 @@ function chargerQuestionnaire(json){
 	for(i=0;i<json.questions.length;i++){
 		html += '<blocQR class="blocQR"> \
 		<p class="question"> \
-		Question: <input type="text" name="question" class="questionInput inputText inputButton" value="'+json.questions[i].texte+'"/> \
+		Question: <input type="text" id="question'+i+'" name="question" class="questionInput inputText inputButton" value="'+json.questions[i].texte+'"/> \
+		<a class="linkLatex" href="javascript:OpenLatexEditor("question'+i+'","latex","fr-fr")"><img src="src/formula_icon.png"></a> \
 		</p><reponses>';
 		for(j=0;j<json.questions[i].reponses.length;j++){
 			html += '<p class="reponse"> \
-			Reponse: <input type="text" name="reponse" class="reponseInput inputText inputButton" value="'+json.questions[i].reponses[j].texte+'"/> \
+			Reponse: <input type="text" id="reponse '+i+','+j+'" name="reponse" class="reponseInput inputText inputButton" value="'+json.questions[i].reponses[j].texte+'"/> \
+			<a class="linkLatex" href="javascript:OpenLatexEditor("reponse '+i+','+j+'","latex","fr-fr")"><img src="src/formula_icon.png"></a> \
 			<span class="checkbox">Bonne reponse?<input class="bonneInput" type="checkbox" name="bonne"';
 			if(json.questions[i].reponses[j].correcte){html+=' checked="true"';}
 			html+='"/></span> \
