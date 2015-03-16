171927a4efad73e879d776d23b0e7301deed3f93
Julien LANGLOIS <julien.langlois@eleves.ec-nantes.fr>
2015-03-16 18:10:53 +0100

    mise à jour des redirections JS sur les fichiers html en redirection sur les php

--- WebContent/js/correction.js
+++ WebContent/js/correction.js
@@ -419,7 +419,7 @@ function lancerCorrection(callback){
 };
 function getCorrectionStatus(code){
 	if(code=='1'){
-		 document.location.href="Correction.html";
+		 document.location.href="Correction.php";
 	}else{
 		showMessage("error",code);
 	}
